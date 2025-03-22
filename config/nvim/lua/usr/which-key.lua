847e98e8a3353244cc0c34b8c44139e8c57d610d
wsxq2 <wsxq222222@gmail.com>
2025-03-22 20:22:32 +0800

    update

--- config/nvim/lua/usr/which-key.lua
+++ config/nvim/lua/usr/which-key.lua
@@ -1,10 +1,24 @@
 -- whichkey configuration
 local wk = require("which-key")
 wk.setup({
-  plugins = {
-    marks = true, -- shows a list of your marks on ' and `
-    registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mo
-  },
+    preset = "classic",
+    plugins = {
+        marks = true, -- shows a list of your marks on ' and `
+        registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mo
+        spelling = {
+            enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+            suggestions = 20, -- how many suggestions should be shown in the list?
+        },
+        presets = {
+            operators = false, -- adds help for operators like d, y, ...
+            motions = false, -- adds help for motions
+            text_objects = false, -- help for text objects triggered after entering an operator
+            windows = false, -- default bindings on <c-w>
+            nav = false, -- misc bindings to work with windows
+            z = false, -- bindings for folds, spelling and others prefixed with z
+            g = false, -- bindings for prefixed with g
+        },
+    },
 })
 
 wk.add({
