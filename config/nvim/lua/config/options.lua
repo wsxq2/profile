37cd55b2fa79bcad63860cb409b3ddeb766540c0
wsxq2 <wsxq222222@gmail.com>
2025-03-23 21:08:13 +0800

    disable spell check default for md file written

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+opt.spelllang = { "en", "cjk" }
+opt.spell = false -- Not enable spell checking
+opt.spellfile = vim.fn.stdpath("config") .. "/nvim/spell/zh.utf-8.add" -- Custom spell file
